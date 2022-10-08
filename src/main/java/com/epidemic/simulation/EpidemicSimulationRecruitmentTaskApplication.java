4506b69b7d0eeb449573e89139a1995b1d0d1219
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-10-08 12:18:15 +0200

    First commit

---
+++ src/main/java/com/epidemic/simulation/EpidemicSimulationRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package com.epidemic.simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpidemicSimulationRecruitmentTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpidemicSimulationRecruitmentTaskApplication.class, args);
+    }
+
+}
