905bdecf6940dccbeb188b8fe084668e105c048b
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-10-13 02:06:08 +0200

    Created repositories for simulations and reports

---
+++ src/main/java/com/epidemic/simulation/repository/ReportRepository.java
@@ -0,0 +1,12 @@
+package com.epidemic.simulation.repository;
+
+import com.epidemic.simulation.domain.Report;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends CrudRepository<Report, Long> {
+
+    @Override
+    Report save(Report report);
+}
---
+++ src/main/java/com/epidemic/simulation/repository/SimulationRepository.java
@@ -0,0 +1,12 @@
+package com.epidemic.simulation.repository;
+
+import com.epidemic.simulation.domain.Simulation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SimulationRepository extends CrudRepository<Simulation, Long> {
+
+    @Override
+    Simulation save(Simulation simulation);
+}
